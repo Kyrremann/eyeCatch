5b38743bd33258ad966dcf6761be81ab45df3232
Kyrre Havik Eriksen <kyrremann@gmail.com>
2013-09-20 22:56:32 +0200

    Started working on the training and test levels. Not completed yet.

--- src/no/minimon/eyecatch/EyeCatchGameActivity.java
+++ src/no/minimon/eyecatch/EyeCatchGameActivity.java
@@ -3,7 +3,6 @@ package no.minimon.eyecatch;
 import java.util.Random;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
@@ -21,16 +20,20 @@ public class EyeCatchGameActivity extends FragmentActivity {
 	private SparseArray<Drawable> faces;
 	private ImageView faceImageView;
 	private Random random;
+	private int TESTING_LEVEL = 0;
+	private int TRAINING_LEVEL = 0;
+	private int face_range = 0;
+	private boolean testingLevel = false;
 
 	public static final int RESULT_VIDEOVIEW = 256;
-	public static final int NORTH = 0;
-	public static final int NORTH_EAST = 1;
-	public static final int EAST = 2;
-	public static final int SOUTH_EAST = 3;
-	public static final int SOUTH = 4;
-	public static final int NORTH_WEST = 5;
-	public static final int WEST = 6;
-	public static final int SOUTH_WEST = 7;
+	public static final int WEST = 0;
+	public static final int EAST = 1;
+	public static final int NORTH = 2;
+	public static final int SOUTH = 3;
+	public static final int NORTH_WEST = 4;
+	public static final int NORTH_EAST = 5;
+	public static final int SOUTH_WEST = 6;
+	public static final int SOUTH_EAST = 7;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -61,29 +64,29 @@ public class EyeCatchGameActivity extends FragmentActivity {
 			switch (view.getId()) {
 			case R.id.image_face:
 				break;
-			case R.id.image_north:
-				checkForCorrectDirection(NORTH);
-				break;
-			case R.id.image_north_east:
-				checkForCorrectDirection(NORTH_EAST);
+			case R.id.image_west:
+				checkForCorrectDirection(WEST);
 				break;
 			case R.id.image_east:
 				checkForCorrectDirection(EAST);
 				break;
-			case R.id.image_south_east:
-				checkForCorrectDirection(SOUTH_EAST);
+			case R.id.image_north:
+				checkForCorrectDirection(NORTH);
 				break;
 			case R.id.image_south:
 				checkForCorrectDirection(SOUTH);
 				break;
+			case R.id.image_north_west:
+				checkForCorrectDirection(NORTH_WEST);
+				break;
+			case R.id.image_north_east:
+				checkForCorrectDirection(NORTH_EAST);
+				break;
 			case R.id.image_south_west:
 				checkForCorrectDirection(SOUTH_WEST);
 				break;
-			case R.id.image_west:
-				checkForCorrectDirection(WEST);
-				break;
-			case R.id.image_north_west:
-				checkForCorrectDirection(NORTH_WEST);
+			case R.id.image_south_east:
+				checkForCorrectDirection(SOUTH_EAST);
 				break;
 			default:
 				updateFaceWithNewImage();
@@ -98,8 +101,6 @@ public class EyeCatchGameActivity extends FragmentActivity {
 		if (CURRENT_FACE == direction) {
 			startActivityForResult(new Intent(this, VideoViewActivity.class),
 					RESULT_VIDEOVIEW);
-			// Videotime
-			// updateFaceWithNewImage();
 		} else {
 			// Wrong, buzz and new face
 		}
@@ -117,12 +118,107 @@ public class EyeCatchGameActivity extends FragmentActivity {
 
 	private void initGame() {
 		GAME_ON = true;
-		/* TODO: Start game
-		 - Pick/load level
-		 - Load video
-		 - Start game
-		 */
-		updateFaceWithNewImage();
+		/*
+		TODO: Start game
+		- Pick/load level
+		- Load video
+		- Start game
+		*/loadLevelOrTesting();
+	}
+
+	private void loadLevelOrTesting() {
+		showAllBoxes();
+		if (testingLevel) {
+			switch (TESTING_LEVEL) {
+			case 0:
+				faceImageView.setImageDrawable(getResources().getDrawable(
+						R.drawable.mariama_center_brighted));
+				break;
+			case 1:
+				face_range = 1;
+				updateFaceWithNewImage();
+				hideRestOfTheBoxes();
+				break;
+			default:
+				break;
+			}
+		} else {
+			switch (TRAINING_LEVEL) {
+			case 0:
+				faceImageView.setImageDrawable(getResources().getDrawable(
+						R.drawable.mariama_center));
+				break;
+			case 1:
+				face_range = 1;
+				break;
+
+			default:
+				break;
+			}
+		}
+		testingLevel = !testingLevel;
+	}
+
+	private void showAllBoxes() {
+		ImageView imageView = (ImageView) findViewById(R.id.image_west);
+		imageView.setVisibility(View.VISIBLE);
+		imageView = (ImageView) findViewById(R.id.image_east);
+		imageView.setVisibility(View.VISIBLE);
+		imageView = (ImageView) findViewById(R.id.image_north);
+		imageView.setVisibility(View.VISIBLE);
+		imageView = (ImageView) findViewById(R.id.image_south);
+		imageView.setVisibility(View.VISIBLE);
+		imageView = (ImageView) findViewById(R.id.image_north_west);
+		imageView.setVisibility(View.VISIBLE);
+		imageView = (ImageView) findViewById(R.id.image_north_east);
+		imageView.setVisibility(View.VISIBLE);
+		imageView = (ImageView) findViewById(R.id.image_south_east);
+		imageView.setVisibility(View.VISIBLE);
+		imageView = (ImageView) findViewById(R.id.image_south_east);
+		imageView.setVisibility(View.VISIBLE);
+	}
+
+	private void hideRestOfTheBoxes() {
+		for (int i = 0; i < faces.size(); i++) {
+			if (i == CURRENT_FACE) {
+				continue;
+			}
+			ImageView imageView;
+			switch (i) {
+			case WEST:
+				imageView = (ImageView) findViewById(R.id.image_west);
+				imageView.setVisibility(View.INVISIBLE);
+				break;
+			case EAST:
+				imageView = (ImageView) findViewById(R.id.image_east);
+				imageView.setVisibility(View.INVISIBLE);
+				break;
+			case NORTH:
+				imageView = (ImageView) findViewById(R.id.image_north);
+				imageView.setVisibility(View.INVISIBLE);
+				break;
+			case SOUTH:
+				imageView = (ImageView) findViewById(R.id.image_south);
+				imageView.setVisibility(View.INVISIBLE);
+				break;
+			case NORTH_WEST:
+				imageView = (ImageView) findViewById(R.id.image_north_west);
+				imageView.setVisibility(View.INVISIBLE);
+				break;
+			case NORTH_EAST:
+				imageView = (ImageView) findViewById(R.id.image_north_east);
+				imageView.setVisibility(View.INVISIBLE);
+				break;
+			case SOUTH_WEST:
+				imageView = (ImageView) findViewById(R.id.image_south_east);
+				imageView.setVisibility(View.INVISIBLE);
+				break;
+			case SOUTH_EAST:
+				imageView = (ImageView) findViewById(R.id.image_south_east);
+				imageView.setVisibility(View.INVISIBLE);
+				break;
+			}
+		}
 	}
 
 	private void updateFaceWithNewImage() {
@@ -130,19 +226,19 @@ public class EyeCatchGameActivity extends FragmentActivity {
 	}
 
 	private Drawable getRandomFace() {
-		CURRENT_FACE = random.nextInt(7);
+		CURRENT_FACE = random.nextInt(face_range);
 		return faces.get(CURRENT_FACE);
 	}
 
 	private void loadImagesIntoFaces() {
-		faces.put(NORTH, getResources().getDrawable(R.drawable.mariama_n));
-		faces.put(NORTH_EAST, getResources().getDrawable(R.drawable.mariama_ne));
-		faces.put(EAST, getResources().getDrawable(R.drawable.mariama_e));
-		faces.put(SOUTH_EAST, getResources().getDrawable(R.drawable.mariama_se));
-		faces.put(SOUTH, getResources().getDrawable(R.drawable.mariama_s));
-		faces.put(SOUTH_WEST, getResources().getDrawable(R.drawable.mariama_sw));
 		faces.put(WEST, getResources().getDrawable(R.drawable.mariama_w));
+		faces.put(EAST, getResources().getDrawable(R.drawable.mariama_e));
+		faces.put(NORTH, getResources().getDrawable(R.drawable.mariama_n));
+		faces.put(SOUTH, getResources().getDrawable(R.drawable.mariama_s));
 		faces.put(NORTH_WEST, getResources().getDrawable(R.drawable.mariama_nw));
+		faces.put(NORTH_EAST, getResources().getDrawable(R.drawable.mariama_ne));
+		faces.put(SOUTH_WEST, getResources().getDrawable(R.drawable.mariama_sw));
+		faces.put(SOUTH_EAST, getResources().getDrawable(R.drawable.mariama_se));
 	}
 
 	@Override
