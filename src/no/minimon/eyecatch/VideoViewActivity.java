621b12b04fe2481002a9f6ed7ef61517bc994d93
Kyrre Havik Eriksen <kyrremann@gmail.com>
2014-01-25 23:24:41 +0100

    Fikset bug der vidoen var kortere enn threshold, som gjorde at appen hang i en evig løkke

--- src/no/minimon/eyecatch/VideoViewActivity.java
+++ src/no/minimon/eyecatch/VideoViewActivity.java
@@ -23,7 +23,7 @@ public class VideoViewActivity extends FragmentActivity {
 	private static final String LOG_VV = "VIDEOVIEW";
 	private VideoView videoView;
 	private Random random;
-	private boolean endGame;
+	private boolean endGame, video_shorter_then_threshold;
 
 	@Override
 	protected void onCreate(Bundle savedInstance) {
@@ -40,28 +40,46 @@ public class VideoViewActivity extends FragmentActivity {
 				.getDurationOnCurrentVideo(this);
 		int threshold = SharedPreferencesUtil.getAllowedVideoDuration(this);
 		int lastSeek = SharedPreferencesUtil.getLastSeekOnCurrentVideo(this);
+		Log.d(LOG_VV, "Lastseek: " + lastSeek);
+		Log.d(LOG_VV, "Threshold: " + threshold);
+		Log.d(LOG_VV, "Duration of video: " + duration_of_video);
 
 		threshold = lastSeek + threshold;
 		if (threshold > duration_of_video) {
-			if (endGame) {
+			if (endGame) { // you have already seen the whole video
 				setResult(EyeCatchGameActivity.RESULT_ENDGAME);
 				finish();
+			} else {
+				video_shorter_then_threshold = true;
 			}
 			threshold = duration_of_video;
-			threshold -= 250; // to avoid bad timing
-			// TODO Check to see if video has been played
+			Log.d(LOG_VV, "Updated threshold to " + threshold);
 		}
 
 		videoView.setVideoURI(Uri.parse(uri));
 		videoView.setZOrderOnTop(true);
 
-		if (!endGame) {
+		if (video_shorter_then_threshold) {
+			videoView.start();
+			videoView.seekTo(lastSeek);
+			videoView.setOnCompletionListener(new OnCompletionListener() {
+
+				@Override
+				public void onCompletion(MediaPlayer arg0) {
+					SharedPreferencesUtil.setLastSeekOnCurrentVideo(getApplication(),
+							0);
+					Log.d(LOG_VV, "Seek saved at " + 0);
+					setResult(EyeCatchGameActivity.RESULT_VIDEOVIEW);
+					finish();
+				}
+			});
+		} else if (!endGame) {
 			new ASync().execute(duration_of_video, lastSeek, threshold);
 		} else {
 			videoView.start();
 			videoView.seekTo(lastSeek);
 			videoView.setOnCompletionListener(new OnCompletionListener() {
-				
+
 				@Override
 				public void onCompletion(MediaPlayer arg0) {
 					setResult(EyeCatchGameActivity.RESULT_ENDGAME);
@@ -92,13 +110,14 @@ public class VideoViewActivity extends FragmentActivity {
 			}
 
 			lastSeek = videoView.getCurrentPosition();
+			Log.d(LOG_VV, "Video stopped at " + lastSeek);
 			if (lastSeek < 0 || lastSeek > duration - 1000) {
 				lastSeek = 0;
 			}
 			videoView.stopPlayback();
 			SharedPreferencesUtil.setLastSeekOnCurrentVideo(getApplication(),
 					lastSeek);
-			Log.d(LOG_VV, "Video stopped at " + lastSeek);
+			Log.d(LOG_VV, "Seek saved at " + lastSeek);
 			setResult(EyeCatchGameActivity.RESULT_VIDEOVIEW);
 			finish();
 
